7cb15a6d83e09ffa43c12b8f81c501772750dade
s.morjane <safa.morjane@gmail.com>
2023-01-19 21:48:18 +0100

    add template components + meta tags

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
