f3b87e6a85a02cd100557f48714829d562900354
s.morjane <safa.morjane@gmail.com>
2023-01-20 17:54:06 +0100

    homepage main content style changes

--- src/components/Homepage/components/Main/index.jsx
+++ src/components/Homepage/components/Main/index.jsx
@@ -11,19 +11,22 @@ export default function Main() {
       image: GreekSalad,
       title: 'Greek Salad',
       price: '$12.99',
-      description: 'The famous greek salad',
+      description:
+        'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.',
     },
     {
       image: Bruschetta,
       title: 'Bruchetta',
       price: '$5.99',
-      description: 'Our Bruschetta',
+      description:
+        'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.',
     },
     {
       image: LemonDesert,
       title: 'Lemon Desert',
       price: '$5.00',
-      description: 'This comes',
+      description:
+        "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
     },
   ];
   return (
--- src/components/Homepage/styled.js
+++ src/components/Homepage/styled.js
@@ -7,7 +7,7 @@ export const HeaderContainer = styled.header`
   align-items: center;
   justify-content: center;
   gap: 10%;
-  padding: 0px 4%;
+  padding: 0px 1%;
   height: 30vh;
   * {
     width: 220px;
@@ -40,30 +40,42 @@ export const Content = styled.div`
 export const Headline = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
-  gap: 140px;
+  justify-content: space-between;
+  width: 100%;
 `;
 export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 60px 4%;
   gap: 30px;
+  min-width: 630px;
+  width: 33vw;
   height: 50vh;
+  margin: auto;
+  padding-top: 3%;
 `;
 export const Section = styled.section`
   display: flex;
   flex-direction: column;
-  background-color: #e1e1e1;
+  background-color: aliceblue;
   border-radius: 16px;
-  gap: 20px;
+  width: 30%;
+  padding-bottom: 10px;
+  div,
+  p,
+  a {
+    margin: 10px 16px;
+  }
   div {
     display: flex;
     justify-content: space-between;
   }
   img {
-    border-radius: 16px;
-    width: 200px;
+    border-top-left-radius: 16px;
+    border-top-right-radius: 16px;
+    object-fit: cover;
+    width: 100%;
+    height: 152px;
   }
   h6 {
     color: orange;
@@ -72,10 +84,21 @@ export const Section = styled.section`
   h4 {
     font-size: medium;
   }
+
+  p {
+    font-size: smaller;
+  }
+  a {
+    text-decoration: none;
+    font-weight: 600;
+    color: black;
+    margin-top: auto;
+  }
 `;
 
 export const Specials = styled.div`
   display: flex;
   justify-content: space-between;
-  gap: 30px;
+  width: 100%;
+  height: 100%;
 `;
